3c6cc0bc03a7cb62375222ebf918f9e3d79fd63c
Anton Zotov <votoznotna@gmail.com>
2017-08-27 16:36:13 -0700

    added smoothing to header sticking.

--- controllers/menu.js
+++ controllers/menu.js
@@ -6,17 +6,24 @@
 
    function menuCtrl($scope, $state, $timeout, CONSTANTS) {
       $scope.cvRef = CONSTANTS.cvRef;
-
+      $('#menu-phantom').html($('#menu').html());
       $(window).scroll(function () {
+         var window_top = $(window).scrollTop();
+         var div_top = $('#content-anchor').offset().top;
          var profileTitle = $('#profileTitle'),
             menu = $('#menu'),
-            windowScrollTop = $(window).scrollTop(),
-            profileTitleHeight = profileTitle.height();
-         if (windowScrollTop > profileTitleHeight) {
+            menuPhantom = $('#menu-phantom');
+            //menuPhantom.html(menu.html());
+         if (window_top > div_top) {
             menu.addClass('navbar-fixed-top');
-         }
-         if (windowScrollTop <= profileTitleHeight) {
+            //menu.removeClass('zindex0').addClass('zindex1');
+            menuPhantom.show();
+            //menuPhantom.removeClass('zindex1').addClass('zindex0');
+         } else {
             menu.removeClass('navbar-fixed-top');
+            //menu.removeClass('zindex0').addClass('zindex1');
+            menuPhantom.hide();
+            //menuPhantom.removeClass('zindex1').addClass('zindex0');
          }
       });
 
