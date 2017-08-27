0d26cbde8965c375a4f8475ad6e0c66816eec0c3
Anton Zotov <votoznotna@gmail.com>
2017-08-27 13:35:49 -0700

    added sticker menu.

--- controllers/menu.js
+++ controllers/menu.js
@@ -6,5 +6,19 @@
 
    function menuCtrl($scope, $state, $timeout, CONSTANTS) {
       $scope.cvRef = CONSTANTS.cvRef;
+
+      $(window).scroll(function () {
+         var profileTitle = $('#profileTitle'),
+            menu = $('#menu'),
+            windowScrollTop = $(window).scrollTop(),
+            profileTitleHeight = profileTitle.height();
+         if (windowScrollTop > profileTitleHeight) {
+            menu.addClass('navbar-fixed-top');
+         }
+         if (windowScrollTop <= profileTitleHeight) {
+            menu.removeClass('navbar-fixed-top');
+         }
+      });
+
    }
 }(angular.module("iZotova")));
