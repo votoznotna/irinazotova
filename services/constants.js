21f4deca9c6ab17cf14858f3ab2c833647864979
Anton Zotov <votoznotna@gmail.com>
2017-09-07 18:46:32 -0700

    pdf link update.

--- services/constants.js
+++ services/constants.js
@@ -2,6 +2,6 @@
    "use strict";
 
    module.constant('CONSTANTS', {
-      cvRef: 'https://drive.google.com/file/d/0BxOmYacCKrBPaTNXYWxqVTAteUk/view'
+      cvRef: 'https://drive.google.com/file/d/0B0RUR516bTY8M2U3dGxkeF93eG8/view'
    })
 }(angular.module('iZotova')));
